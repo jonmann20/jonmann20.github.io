9bdbda4c5bbfc37604e9d2e43913fc95c7950bfc
Jon Wiedmann <jonwiedmann@gmail.com>
2013-05-09 13:49:10 -0300

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,18 +1,8 @@
 var Master = function(){
     // private
     var headContent = "",
-        mainContent = "";
-
-    headContent += "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'> ";
-    headContent += "<meta http-equiv='X-UA-Compatible' content='chrome=1' />";
-    headContent += "<meta name='description' content='Jonmann20.github.com : ' />";
-    headContent += "<meta name='keywords' content='' />";
-    headContent += "<meta name='viewport' content='width=device-width' />";
-    headContent += "<link href='//fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>";
-    headContent += "<link rel='icon' type='image/ico' href='/img/favicon.ico'>";
-    
-    headContent += "<link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>";
-    headContent += "<title>Jonmann20.github.com</title>";
+        mainContent = "",
+        pageRoot = "";
 
     // public
     return {
@@ -22,9 +12,25 @@ var Master = function(){
         main: function(str){    
             mainContent = str;
         },
+        root: function(str){
+            pageRoot = str;
+        },
         init: function(){
             $('head').html(headContent);
             $('main').append(mainContent);
+        },
+        
+        setHead: function(){
+            headContent += "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'> ";
+            headContent += "<meta http-equiv='X-UA-Compatible' content='chrome=1' />";
+            headContent += "<meta name='description' content='Jonmann20.github.com : ' />";
+            headContent += "<meta name='keywords' content='' />";
+            headContent += "<meta name='viewport' content='width=device-width' />";
+            headContent += "<link href='//fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>";
+            headContent += "<link rel='icon' type='image/ico' href='" + pageRoot + "/img/favicon.ico'>";
+            
+            headContent += "<link rel='stylesheet' type='text/css' media='screen' href='" + pageRoot + "css/style.css'>";
+            headContent += "<title>Jonmann20.github.com</title>";
         },
         
         /* Utilities */
