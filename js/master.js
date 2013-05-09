9103da74523d3050042ec142c0f4d34c3d75ecd5
Jon Wiedmann <jonwiedmann@gmail.com>
2013-05-09 13:12:38 -0300

    Update master.js

--- js/master.js
+++ js/master.js
@@ -6,17 +6,13 @@ var Master = function(){
     headContent += "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'> ";
     headContent += "<meta http-equiv='X-UA-Compatible' content='chrome=1' />";
     headContent += "<meta name='description' content='Jonmann20.github.com : ' />";
+    headContent += "<meta name='keywords' content='' />";
+    headContent += "<meta name='viewport' content='width=device-width' />";
+    headContent += "<link href='//fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>";
+    headContent += "<link rel='icon' type='image/ico' href='/img/favicon.ico'>";
     
     headContent += "<link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>";
     headContent += "<title>Jonmann20.github.com</title>";
-
-
-    //<meta name='keywords' content='<?= $pageKeywords; ?>' />
-    //<meta name='viewport' content='width=device-width' />
-
-	//<link href='//fonts.googleapis.com/css?family=Open+Sans|Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>	
-    //<link rel='icon' type='image/ico' href='<?= $pageRoot; ?>img/favicon.ico'>
-
 
     // public
     return {
