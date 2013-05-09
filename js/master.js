cf2ed99dd2f9c90c9ebd20bf6758d416b1f9b64d
Jon Wiedmann <jonwiedmann@gmail.com>
2013-05-09 12:43:30 -0300

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,12 +1,12 @@
 var Master = function(){
     // private
     var headContent = "
-        <meta charset='utf-8' />
-        <meta http-equiv='X-UA-Compatible' content='chrome=1' />
-        <meta name='description' content='Jonmann20.github.com : ' />
-        
-        <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
-        <title>Jonmann20.github.com</title>
+        <meta charset='utf-8' />\
+        <meta http-equiv='X-UA-Compatible' content='chrome=1' />\
+        <meta name='description' content='Jonmann20.github.com : ' />\
+        \
+        <link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>\
+        <title>Jonmann20.github.com</title>\
     ",
         mainContent = "";
 
