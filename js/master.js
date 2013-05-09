b5f0d69cfdaaabf9d9f13038d27033765e45e5e9
Jon Wiedmann <jonwiedmann@gmail.com>
2013-05-09 11:44:14 -0300

    Create master.js

---
+++ js/master.js
@@ -0,0 +1,17 @@
+// plugins/libraries
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+
+var Master = function(){
+    var main = "";
+
+    return {
+        init: function(){
+            $(document.body).append(main);
+        }
+    };
+}();
+
+$(function(){
+    Master.init();
+});
