32689f1344a21f87f101f9eec9203acfb0f64d37
jon <jon@jon-Inspiron-660s>
2013-05-16 19:35:42 -0400

    a

--- games/gamesMaster.php
+++ games/gamesMaster.php
@@ -7,13 +7,13 @@ $pageHeader = isset($pageHeader) ? $pageHeader : '';
 <!DOCTYPE html>
 <html>  
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+	<meta charset='utf-8'>
     <title><?= $pageTitle; ?></title>
     <meta name='description' content='<?= $pageDescription; ?>' />
     <meta name='keywords' content='<?= $pageKeywords; ?>' />
     <meta name='viewport' content='width=device-width' />
-    
-    <link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Press+Start+2P' rel='stylesheet' type='text/css'>
     <link rel='stylesheet' type='text/css' href='<?= $pageRoot; ?>css/the.css' />
     <link rel='icon' type='image/ico' href='<?= $pageRoot; ?>img/favicon.ico'>
     <?= $pageHeader; ?>
@@ -22,18 +22,5 @@ $pageHeader = isset($pageHeader) ? $pageHeader : '';
 
 	<?= $pageMainContent; ?>
     <?= $pageJs; ?>
-    
-    <?// Google Analytics ?>
-    <script defer async>
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-32866104-1']);
-		_gaq.push(['_trackPageview']);
-			
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	</script>
 </body>
 </html>
