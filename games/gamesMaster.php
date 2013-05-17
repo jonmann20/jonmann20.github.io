cf573d95729c9911879a5fbac10921a8561bae5b
jon <jon@jon-Inspiron-660s>
2013-05-16 21:39:23 -0400

    a

--- games/gamesMaster.php
+++ games/gamesMaster.php
@@ -13,7 +13,7 @@ $pageHeader = isset($pageHeader) ? $pageHeader : '';
     <meta name='keywords' content='<?= $pageKeywords; ?>' />
     <meta name='viewport' content='width=device-width' />
 
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Press+Start+2P' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
     <link rel='stylesheet' type='text/css' href='<?= $pageRoot; ?>css/the.css' />
     <link rel='icon' type='image/ico' href='<?= $pageRoot; ?>img/favicon.ico'>
     <?= $pageHeader; ?>
