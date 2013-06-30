23c3486b61d80558606f747e03e9cbe77eb80ed4
Jon Wiedmann <jonwiedmann@gmail.com>
2013-06-30 15:04:27 -0400

    a

--- games/jonsQuest/js/startScreen.js
+++ games/jonsQuest/js/startScreen.js
@@ -1,6 +1,8 @@
 startScreen = function(){
 	
-	var _time = 0
+	var _time = 0,
+		copyTitle = "JON'S QUEST",
+		copyLine = "© 2013 JON WIEDMANN"
 	
 	function update(){
 		if(13 in keysDown){
@@ -16,11 +18,22 @@ startScreen = function(){
 		ctx.fillRect(0,0,FULLW, FULLH+game.padHUD)
 		
 		ctx.font = "36px 'Press Start 2P'"
+		
+		// title
+		ctx.fillStyle = "#222"
+		ctx.fillText(copyTitle, HALFW - ctx.measureText(copyTitle).width / 2 + 4, 94)
+		
 		ctx.fillStyle = "#ff6a00"
-		ctx.fillText("JON'S QUEST", HALFW - 190, HALFH - 80)
+		ctx.fillText(copyTitle, HALFW - ctx.measureText(copyTitle).width / 2, 90)
 		
+		// p enter
+		utils.blinkText(25, HALFW, HALFH + 80)
 		
-		utils.blinkText(28, HALFW, HALFH + 80)
+		// copy
+		ctx.font = "13px 'Press Start 2P'"
+		ctx.fillStyle = "#ddd"
+		
+		ctx.fillText(copyLine, HALFW - ctx.measureText(copyLine).width / 2, FULLH + 44)		
 	}
 	
 	return {
