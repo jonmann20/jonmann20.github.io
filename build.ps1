4e8d4cf76b6677f55001b89730bb2be6cfb538d9
Jon Wiedmann <jonwiedmann@gmail.com>
2013-08-28 22:33:51 -0400

    bild

--- build.ps1
+++ build.ps1
@@ -1,113 +1,19 @@
-﻿#---notes
+﻿#--- notes
 #
 #  run in powershell with:
 #         ./build.ps1 <arg(s)>
 
-#--globals
-# $phpExe = "C:\xampp\php\php"
-# $yuiJar = "C:\Program Files (x86)\yuicompressor-2.4.8.jar"
-
+#--- globals
 $path = "C:\git\jonmann20.github.com\"
-
 $yuiJar = "${path}bin\yuicompressor-2.4.8.jar"
 
-# $htmlFiles = gci ${path}"*" *.html
-# $htmlFiles += gci ${path}"portfolio\*" *.html
-# $htmlFiles += gci ${path}"playground\*" *.html
-# $htmlFiles += gci ${path}"music\*" *.html
-# $htmlFiles += gci ${path}"games\*" *.html
-# $htmlFiles += gci ${path}"games\jonsQuest\*" *.html
-# $htmlFiles += gci ${path}"games\dungeon\*" *.html
-# $htmlFiles += gci ${path}"blog\*" *.html
 
-# $phpFiles = gci ${path}"*" *.php
-# $phpFiles += gci ${path}"portfolio\*" *.php
-# $phpFiles += gci ${path}"playground\*" *.php
-# $phpFiles += gci ${path}"music\*" *.php
-# $phpFiles += gci ${path}"games\*" *.php
-# $phpFiles += gci ${path}"games\jonsQuest\*" *.php
-# $phpFiles += gci ${path}"games\dungeon\*" *.php
-# $phpFiles += gci ${path}"blog\*" *.php
-
-# function deleteHTML(){
-    # echo "----- deleting HTML -----"
-     
-    # foreach($f in $htmlFiles) {
-        # $count=0
-        # $name=""
-    
-        # $f.fullname.split("\") | ForEach {
-            
-            # if($count -gt 2) {
-                # $name += "$_/"
-            # }
-            
-            # $count++
-        # }
-        # $name = $name.substring(0, $name.length-1)
-    
-        # Write-Host "`t Deleting" $name -foregroundcolor "green"
-        # Remove-Item $f.fullname
-    # }
-    
-    # echo ""
-# }
-
-# function compilePHP(){
-    # echo "----- compiling PHP to HTML -----"
-    
-    # foreach($f in $phpFIles){
-        # $count=0
-        # $name=""
-    
-        # $f.fullname.split("\") | ForEach {
-            
-            # if($count -gt 2) {
-                # $name += "$_\"
-            # }
-            
-            # $count++
-        # }
-        # $name = $name.substring(0, $name.length-5)
-    
-        # if(($name -eq "master") -or 
-           # ($name -eq "games\gamesMaster") -or
-           # ($name -eq "games\gamesNav") -or
-           # ($name -eq "music\musicNav") -or
-           # ($name -eq "playground\playgroundNav")
-        # ){
-            # Write-Host "`t -Skipped" $name -foregroundcolor "red"
-        # }
-        # else {
-            # Write-Host "`t Compiling" $name -foregroundcolor "green"
-            
-            # #& $phpExe ${path}${name}".php" > ${path}${name}".html"
-            
-            
-            # #--- compile async
-            # $phpFile = $path + $name + ".php" 
-            # $htmlFile = $path + $name + ".html"
-
-            # $job = Start-Job -ScriptBlock {
-               # & $args[0] $args[1] > $args[2]
-            # } -argumentlist @($phpExe, $phpFile, $htmlFile)
-            
-            # Wait-Job $job
-            
-            # #--- convert to utf8
-            # $htmlFileContent = gc $htmlFile
-            # $htmlFileContent | Set-Content -Encoding UTF8 $htmlFile
-        # }
-    # }
-    
-    # echo ""
-# }
-
+#--- combines /index.html css includes
 function compressCSS() {
 	echo "----- Compressing CSS -----";
 	$absCssFiles = @();  #empty array
 	
-    $cssFiles = "normalize.css","the.css";
+    $cssFiles = "normalize.css", "overrideUtils.css", "layout.css";
     
     foreach($f in $cssFiles){
 		Write-Host "`t combining $f";
@@ -123,6 +29,7 @@ function compressCSS() {
 	rm "${path}css/combined.css";
 }
 
+#--- combines /index.html js includes + google analytics
 function compressJS() {
 	echo "----- Compressing JS -----";
 	$absJsFiles = @();  # empty array
@@ -144,19 +51,26 @@ function compressJS() {
 }
 
 
-function pushToGithub($msg) { # $1= commit msg
+function pushToGithub($msg, $dst) {
 	echo "----- Pushing to GitHub -----";
 	
-	if($msg){ 
-		git add -A
-		git commit -m $msg
-    }
-	else{
-		git add -A
-		git commit -m "default push"
-	}
+	Write-Host "`tTracking files";
+	git add -A
 	
-	git push
+	Write-Host "`tCommiting files";
+	git commit -m $msg
+	
+	Write-Host "`tPushing dev branch";
+	git push -u origin dev
+	
+	if($dst -eq "master"){
+		Write-Host "`tPushing master branch";
+		
+		git checkout master
+		git merge dev
+		git push
+		git checkout dev
+	}
 }
 
 
@@ -166,10 +80,9 @@ if($args[0] -eq "prd") {
     compressCSS
 	compressJS
     
-    if($args[1]){
-        pushToGithub $args[1]
+    if($args[1] -and $args[2]){
+        pushToGithub $args[1] $args[2]
     }
-
 }
 elseif($args[0] -eq "css") {
     compressCSS
@@ -178,7 +91,12 @@ elseif($args[0] -eq "js") {
     compressJS
 }
 elseif($args[0] -eq "push"){
-    pushToGithub $args[1]
+	if($args[1]){
+		pushToGithub $args[1] $args[2]
+	}
+	else{
+		echo "must append <commit msg> and <target dest>";
+	}
 }
 else {
     echo "must append 'prd | css | js | push'"
