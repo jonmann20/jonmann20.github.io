b12d336b6a40ebcdef3313153ad8a778733fb3c3
Jon Wiedmann <jonwiedmann@gmail.com>
2013-08-28 22:53:59 -0400

    test

--- build.ps1
+++ build.ps1
@@ -16,17 +16,19 @@ function compressCSS() {
     $cssFiles = "normalize.css", "overrideUtils.css", "layout.css";
     
     foreach($f in $cssFiles){
-		Write-Host "`t combining $f";
+		Write-Host "`t combining $f" -foregroundcolor "cyan";
 		$absCssFiles += "${path}css/$f";
 	}
     
     Get-Content $absCssFiles | Out-File -Encoding UTF8 "${path}css\combined.css";    # concatenate
 	
-	Write-Host "`n`t minifying combined.css --> /min/master.css";
+	Write-Host "`n`t minifying combined.css --> /min/master.css" -foregroundcolor "green";
     iex 'java -jar $yuijar "${path}css\combined.css" > "${path}css\min\master.css"'; # minify
     
-	Write-Host "`t removing combined.css";
+	Write-Host "`t removing combined.css" -foregroundcolor "gray";
 	rm "${path}css/combined.css";
+	
+	echo "";
 }
 
 #--- combines /index.html js includes + google analytics
@@ -37,39 +39,51 @@ function compressJS() {
     $jsFiles = "analytics.js", "/plugins/sammy.js","utils.js", "/models/home.js", "/models/about.js", "/models/contact.js", "/models/blog.js", "/models/games.js", "/models/music.js", "/models/playground.js", "/models/portfolio.js", "routing.js", "main.js";
     
     foreach($f in $jsFiles){
-		Write-Host "`t combining $f";
+		Write-Host "`t combining $f" -foregroundcolor "cyan";
 		$absJsFiles += "${path}js/$f";
 	}
     
     Get-Content $absJsFiles | Out-File -Encoding UTF8 "${path}js\combined.js";    # concatenate
 	
-	Write-Host "`n`t minifying combined.js --> /min/master.js";
+	Write-Host "`n`t minifying combined.js --> /min/master.js" -foregroundcolor "green";
     iex 'java -jar $yuijar "${path}js\combined.js" > "${path}js\min\master.js"';  # minify
     
-	Write-Host "`t removing combined.js";
+	Write-Host "`t removing combined.js" -foregroundcolor "gray";
 	rm "${path}js/combined.js";
+	
+	echo "";
 }
 
 
-function pushToGithub($msg, $dst) {
-	echo "----- Pushing to GitHub -----";
+function pushOrMerge($msg, $dst) {
+	echo "----- Pushing -----";
 	
-	Write-Host "`tTracking files";
-	git add -A
-	
-	Write-Host "`tCommiting files";
-	git commit -m $msg
-	
-	Write-Host "`tPushing dev branch";
-	git push -u origin dev
-	
-	if($dst -eq "master"){
+	if($dst -eq "prd") {
 		Write-Host "`tPushing master branch";
 		
 		git checkout master
 		git merge dev
+		
+		compressCSS
+		compressJS
+	}
+	else {
+		Write-Host "`tPushing dev branch";
+	}
+		
+	Write-Host "`tTracking files";
+	git add -A
+		
+	Write-Host "`tCommiting files";
+	git commit -m $msg
+		
+		
+	if($dst -eq "prd"){	
 		git push
 		git checkout dev
+	}
+	else {
+		git push -u origin dev
 	}
 }
 
@@ -77,12 +91,12 @@ function pushToGithub($msg, $dst) {
 #--- Check Command Line Arguments
 
 if($args[0] -eq "prd") {
-    compressCSS
-	compressJS
-    
-    if($args[1] -and $args[2]){
-        pushToGithub $args[1] $args[2]
+    if($args[1]){
+        pushOrMerge $args[1] "prd"
     }
+	else {
+		Write-Host "Must append commit message" -foregroundcolor "red";
+	}
 }
 elseif($args[0] -eq "css") {
     compressCSS
@@ -90,14 +104,15 @@ elseif($args[0] -eq "css") {
 elseif($args[0] -eq "js") {
     compressJS
 }
-elseif($args[0] -eq "push"){
+elseif($args[0] -eq "dev") {
 	if($args[1]){
-		pushToGithub $args[1] $args[2]
+		pushOrMerge $args[1] ""
 	}
-	else{
-		echo "must append <commit msg> and <target dest>";
+	else {
+		Write-Host "Must append commit message" -foregroundcolor "red";
 	}
 }
 else {
-    echo "must append 'prd | css | js | push'"
+    compressCSS
+	compressJS
 }
