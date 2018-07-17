89352f21b233954e1f168b516d075faa732c6d8e
Jon Wiedmann <jonwiedmann@gmail.com>
2018-07-17 13:41:24 -0400

    Fix broken dungeon images

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 1525919401800;
+const CACHE_VERSION = 1531849137653;
 let CURRENT_CACHES = {
 	offline: 'offline-v' + CACHE_VERSION
 };
