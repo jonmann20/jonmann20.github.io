2cfa08873bc24ac8cc3d6e9478c08ced1a5e594f
Jon Wiedmann <jonwiedmann@gmail.com>
2018-08-16 17:37:00 -0400

    Assume sw

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 1531855708228;
+const CACHE_VERSION = 1534455309014;
 let CURRENT_CACHES = {
 	offline: 'offline-v' + CACHE_VERSION
 };
