34793fd1652d9d4285c616dd3b73d3b0d92ac4ec
Jon Wiedmann <jonwiedmann@gmail.com>
2019-01-23 09:00:46 -0500

    try adding all node modules

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 1548222047277;
+const CACHE_VERSION = 1548251789280;
 let CURRENT_CACHES = {
 	offline: 'offline-v' + CACHE_VERSION
 };
